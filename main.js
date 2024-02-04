6ee808ac28290fa500272090a0eba01ae6f93678
rana zaid <itszaidrajpoot@gmail.com>
2024-02-04 21:01:47 +0500

    add some code

--- main.js
+++ main.js
@@ -10,3 +10,15 @@
 //     }
 //     console.log(count)
 // }
+
+// QNO2 create an arrow function to perform the same task?
+
+// const countvow=(str)=>{
+//     let count=0
+//         for(const char of str){
+//             if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
+//                 count++
+//             }
+//         }
+//         return count;
+// }
