56c6785f042c84f1d8b3e1a1fb4ff814ec3b4c2e
rana zaid <itszaidrajpoot@gmail.com>
2024-03-22 22:48:37 +0500

    add some code

--- main.js
+++ main.js
@@ -22,3 +22,47 @@
 //         }
 //         return count;
 // }
+
+// QNO3 write a program to print the marks of a students in an object using for loop ?
+
+// let marks = {
+//     zaid : 90,
+//     Ahmad :95,
+//     Nasir : 96,
+//     Rabi : 89
+// }
+
+// for(let i=0;i<Object.keys(marks).length;i++ ){
+//   console.log("The marks of " + Object.keys(marks)[i] + "is" + marks[Object.keys(marks)[i]])
+// }
+
+// QNO4 Do the same programe in for in loop?
+
+// let marks = {
+//     zaid : 90,
+//     Ahmad :95,
+//     Nasir : 96,
+//     Rabi : 89
+// }
+
+// for(let key in marks ){
+//     console.log("The marks of " + key + "is" + marks[key])
+//   }
+
+// QNO5 write a program to print try again until the user enter the correct number?
+
+// let cn =7
+// let i
+
+// while(i!=cn){
+//     i= prompt("Enter the number   ")
+// }
+// console.log("you enter the correct the number")
+
+
+// QNO6 write a function to find mean of numbers?
+
+// const mean = (a,b,c,d)=>{
+//  return(a+b+c+d)/4
+// }
+// console.log(mean(4,5,6,7))
